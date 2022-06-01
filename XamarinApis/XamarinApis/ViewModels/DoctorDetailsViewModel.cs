a985b6c9c71116e1fcee9ae933f31642eee06e4e
serraguti <pacoserranox@gmail.com>
2022-06-01 13:19:08 +0200

    commit

--- XamarinApis/XamarinApis/ViewModels/DoctorDetailsViewModel.cs
+++ XamarinApis/XamarinApis/ViewModels/DoctorDetailsViewModel.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 using XamarinApis.Base;
 using XamarinApis.Models;
+using XamarinApis.Services;
 
 namespace XamarinApis.ViewModels
 {
     public class DoctorDetailsViewModel: ViewModelBase
     {
+        private ServiceApiDoctores service;
+
+        public DoctorDetailsViewModel(ServiceApiDoctores service)
+        {
+            this.service = service;
+        }
+        public Command DeleteDoctor
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await this.service.DeleteDoctorAsync(Doctor.IdDoctor);
+                    await
+                    Application.Current.MainPage.Navigation.PopModalAsync();
+                });
+            }
+        }
+
         private Doctor _Doctor;
 
         public Doctor Doctor
